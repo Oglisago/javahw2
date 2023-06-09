1e304f750f0bd9d6f3a943e6da1a29eb9506352c
Liza go <elizaveta2000333@gmail.com>
2023-06-09 22:21:00 +0300

    task

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        int amount = 13676;
+        int PerMile = 20;
+        int BonusMiles = amount / PerMile;
+
+        System.out.println("Количество бонусных миль: " + BonusMiles);
+        // другую для хранения количества
+        // рублей для одной бонусной мили
+
+        // Рассчитываете количество бонусных миль, используя
+        // значения заведённых переменных. Ответ сохраняете в
+        // новую переменную и выводите на экран
+    }
+}
